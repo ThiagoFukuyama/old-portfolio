5caf2b9fdd02e619a1fa5d387562dcdae09e56e6
Thiago <thiagofkm@gmail.com>
2022-07-18 22:15:45 -0300

    18/07/2022

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,45 +16,31 @@ nav_btns.forEach((btn) => {
     })
 })
 
-const scrolldown_btn = document.querySelector(".scroll-down-btn") 
-
 
 
 /**
- * Tornar links da Navbar ativos ao scroll
-*/
-
-const nav_items = nav_list.querySelectorAll(".navbar__item");
-const nav_links = nav_list.querySelectorAll(".navbar__link");
+ * Tornar links da Navbar ativos
+ */
 
 const activate_links = () => {
+    const navbar_links = document.querySelectorAll(".navbar__link")
     let screen_position = window.scrollY + 200
-    nav_links.forEach((link) => {
-        const id = link.getAttribute("href")
+    
+    navbar_links.forEach((link) => {
+        const id = link.getAttribute("href").replace("/", "")
         const section = document.querySelector(id)
 
-        link.addEventListener("click", (e) => {
-            e.preventDefault()
-            nav_list.classList.remove("active")
-            scroll_to_target(section)
-        })
-    
+        link.addEventListener("click", () => nav_list.classList.remove("active"))
         if (screen_position >= section.offsetTop - 100 && screen_position <= ((section.offsetTop - 100) + section.offsetHeight)) {
-            link.parentElement.classList.add("active")
+            link.classList.add("active")
         } else {
-            link.parentElement.classList.remove("active")
+            link.classList.remove("active")
         }
     })
 }
 
-// Função de scroll
-
-const scroll_to_target = (target) => {
-    scrollTo(0, target.offsetTop - 50)
-}
-
-window.addEventListener("scroll", activate_links)
 window.addEventListener("load", activate_links)
+window.addEventListener("scroll", activate_links)
 
 
 
@@ -69,14 +55,13 @@ const animate = () => {
     let screen_position = window.innerHeight
     animation_elements.forEach((elemento) => {
         let element_position = elemento.getBoundingClientRect().top
-
-        if (element_position < screen_position - 200) {
+        if (element_position < screen_position - 100) {
             elemento.classList.add("animate")
-        }
+        } 
     })
 
     let ac_position = animation_children.getBoundingClientRect().top
-    if (ac_position < screen_position - 200) {
+    if (ac_position < screen_position - 100) {
         let delay_base = 1
         animation_children.querySelectorAll("[data-animate-children] > *").forEach((child) => {
             child.classList.add("animate")
